69598554f4f081b241e5e64e5635df60aaa0f17d
aymen <jakebi@protonmail.com>
2024-10-09 13:08:07 +0100

    added time check

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const date = new Date();
 
-
-app.get('/', (req,res) => {
+hour = date.getHours();
+day = 6;
+day >= 1 && day <= 5
+  ? hour > 8 && hour < 18
+    ? app.get('/', (req,res) => {
     res.sendFile( __dirname + '/pages/home.html');
 })
+    : app.get('/', (req,res) => {
+        res.send("server don't work")
+  : res.send("server don't work");
+
+
 
 
 app.listen(port, (err)=> 
